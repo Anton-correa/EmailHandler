62978770995230985db02bdba2f30609097fa31a
Anton <tothers77@yahoo.com>
2021-08-10 20:22:32 -0400

    Added more validations to user again

--- src/models/emails.js
+++ src/models/emails.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-var actions = require('../constants')
+const actions = require('../constants')
 const EmailSchema = mongoose.Schema({
     action: {
         type: String,
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,24 +3,31 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const EmailDB = require("../models/emails");
 const validator = require("validator");
+const actions = require('../constants')
 
-router.post("/events", async (req, res) => {
+router.post("/events", (req, res) => {
     //validation
-    const subject = req.query.subject
-    if (validator.isEmail(req.query.recipient)) { 
-      recipient = req.query.recipient;
-    } else {
+    const {subject, recipient, action} = req.query
+    if (validator.isEmpty(recipient) || !validator.isEmail(recipient)) { 
+      res.status(400);
       res.send('<h1>This is not a valid email address format</h1>');
-    }
+      return
+    } 
+
+    if (validator.isEmpty(subject)) { 
+      res.status(400);
+      res.send('<h1>The subject of the email is empty</h1>');
+      return
+    } 
   
     if (
-      validator.equals(req.query.action.toLowerCase(), "click") ||
-      validator.equals(req.query.action.toLowerCase(), "open")
+      validator.isEmpty(action) || !validator.equals(action.toLowerCase(), actions.CLICK) ||
+      !validator.equals(action.toLowerCase(), actions.OPEN)
     ) {
-      action = req.query.action;
-    } else {
+      res.status(400)
       res.send('<h1>Wrong action type, the only acceptable actions are open or click</h1>');
-    }
+      return
+    } 
   
     // save email
     const eventCreate = new EmailDB({
@@ -38,9 +45,18 @@ router.post("/events", async (req, res) => {
       });
   });
   
-  router.get("/events/action/:actionType", async (req, res) => {
-    
-    EmailDB.find({ 'action': req.params.actionType }).sort('recipient').lean()
+  router.get("/events/action/:actionType", (req, res) => {
+    const action = req.params.actionType
+    if (
+      validator.isEmpty(action) || !validator.equals(action.toLowerCase(), actions.CLICK) ||
+      !validator.equals(action.toLowerCase(), actions.OPEN)
+    ) {
+      res.status(400)
+      res.send('<h1>Wrong action type, the only acceptable actions are open or click</h1>');
+      return
+    } 
+
+    EmailDB.find({ 'action': action }).sort('recipient').lean()
     .then((data) => {
       res.render('emails', {emails: data, eventEmail: true})
     }).catch((err) => {
@@ -48,8 +64,14 @@ router.post("/events", async (req, res) => {
     })
   });
 
-  router.get("/events/recipient/:recipientName", async (req, res) => {
-    EmailDB.find({ 'recipient': req.params.recipientName }).sort('recipient').lean()
+  router.get("/events/recipient/:recipientName", (req, res) => {
+    const recipient = req.params.recipientName
+    if (validator.isEmpty(recipient) || !validator.isEmail(recipient)) { 
+      res.status(400);
+      res.send('<h1>This is not a valid email address format</h1>');
+      return
+    } 
+    EmailDB.find({ 'recipient': recipient }).sort('recipient').lean()
     .then((data) => {
       res.render('emails', {emails: data, eventEmail: true})
     }).catch((err) => {
@@ -57,7 +79,7 @@ router.post("/events", async (req, res) => {
     })
   });
 
-  router.get("/events/timestamp/:timestamp", async (req, res) => {
+  router.get("/events/timestamp/:timestamp", (req, res) => {
     EmailDB.find({ 'timestamp': req.params.timestamp }).sort('recipient').lean()
     .then((data) => {
       res.render('emails', {emails: data, eventEmail: true})
@@ -66,14 +88,14 @@ router.post("/events", async (req, res) => {
     })
   });
 
-  router.get("/summary", async (req, res) => {
+  router.get("/summary", (req, res) => {
 
     EmailDB.aggregate([ 
       {
         $group: {
           recipient: "$recipient",
-          openActions: { $count: { $match: { action: "open" } } },
-          clickActions: { $count: { $match: { action: "click" } } },
+          openActions: { $count: { $match: { action: actions.OPEN } } },
+          clickActions: { $count: { $match: { action: actions.CLICK } } },
         },
       },
     ]).then((data) => {
